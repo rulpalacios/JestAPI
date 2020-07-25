f3504ba7b58e1aaa0af5540c237134a35e2355fa
Raul Palacios <palacios.raul02@gmail.com>
2020-07-24 21:18:28 -0500

    all test

--- __test__/users.test.js
+++ __test__/users.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest')
 const app = require('../api')
+const { nanoid } = require('nanoid')
 
 let testServer
 beforeAll(() => {
@@ -18,6 +19,34 @@ describe('GET /api/users', () => {
     expect(response.body).not.toBeNull()
     expect(Array.isArray(response.body.body)).toBe(true)
     expect(response.body.body.length).toBe(2)
+  })
+})
+
+describe('GET /api/users/:id', () => {
+  it('should GET a exercise', async () => {
+    const response = await request(app).get('/api/users/1')
+    expect(response.error).toBe(false)
+    expect(response.status).toBe(200)
+    expect(response.body.body).not.toBeNull()
+    expect(response.body.body.id).toBe("1")
+  })
+})
+
+describe('POST /api/users', () => {
+  it('should POST a new user', async () => {
+    const userId = nanoid()
+    let user = {
+      id: userId,
+      name: 'katya',
+      username: 'katmon'
+    }
+
+    const response = await request(app).post('/api/users').send(user)
+    expect(response.error).toBe(false)
+    expect(response.status).toBe(200)
+    expect(response.body.body).not.toBeNull()
+    expect(response.body.body.id).toBe(userId)
+
   })
 })
 
